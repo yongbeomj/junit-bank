e2594bcce7c02d3bdecca9884a89f0d65e68905a
Yongbeom Jang <alex.ybjang@gmail.com>
2024-06-27 16:01:23 +0900

    회원가입 유효성검사 - 정규표현식 적용

--- src/main/java/shop/mtcoding/bank/dto/user/UserReqDto.java
+++ src/main/java/shop/mtcoding/bank/dto/user/UserReqDto.java
@@ -7,18 +7,31 @@ import shop.mtcoding.bank.domain.user.User;
 import shop.mtcoding.bank.domain.user.UserEnum;
 
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 
 public class UserReqDto {
     @Setter
     @Getter
     public static class JoinReqDto {
+        // 영문, 숫자 가능. 길이 2~20자
         @NotEmpty // null이거나 공백일 수 없다
+        @Pattern(regexp = "^[a-zA-Z0-9]{2,20}$", message = "영문/숫자 2~20자 이내로 작성해주세요")
         private String username;
+
+        // 길이 4~20
         @NotEmpty
+        @Size(min = 4, max = 20) // String 타입만 사용 가능
         private String password;
+
+        // 이메일 형식
         @NotEmpty
+        @Pattern(regexp = "^[a-zA-Z0-9]{2,10}@[a-zA-Z0-9]{2,6}\\.[a-zA-Z]{2,3}$", message = "이메일 형식으로 작성해주세요")
         private String email;
+
+        // 영어, 한글 가능. 길이 1~20
         @NotEmpty
+        @Pattern(regexp = "^[a-zA-Z가-힣]{1,20}$", message = "한글/영문 1~20자 이내로 작성해주세요")
         private String fullname;
 
         // dto to entity
---
+++ src/test/java/shop/mtcoding/bank/temp/RegexTest.java
@@ -0,0 +1,79 @@
+package shop.mtcoding.bank.temp;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.regex.Pattern;
+
+// java.util.regex.Pattern
+public class RegexTest {
+
+    @Test
+    public void 한글만된다_test() {
+        String value = "한글";
+        boolean result = Pattern.matches("^[가-힣]+$", value);
+        System.out.println("테스트 : " + result);
+    }
+
+    @Test
+    public void 한글은안된다_test() {
+        String value = "abc";
+        boolean result = Pattern.matches("^[^ㄱ-ㅎ가-힣]*$", value); // ^: 안에 있으면 not의 의미
+        System.out.println("테스트 : " + result);
+    }
+
+    @Test
+    public void 영어만된다_test() {
+        String value = "ssar";
+        boolean result = Pattern.matches("^[a-zA-Z]+$", value); // ^: 안에 있으면 not의 의미
+        System.out.println("테스트 : " + result);
+    }
+
+    @Test
+    public void 영어는안된다_test() {
+        String value = "가22";
+        boolean result = Pattern.matches("^[^a-zA-Z]*$", value); // ^: 안에 있으면 not의 의미
+        System.out.println("테스트 : " + result);
+    }
+
+    @Test
+    public void 영어와숫자만된다_test() {
+        String value = "ssar2323";
+        boolean result = Pattern.matches("^[a-zA-Z0-9]+$", value); // ^: 안에 있으면 not의 의미
+        System.out.println("테스트 : " + result);
+
+    }
+
+    @Test
+    public void 영어만되고_길이는최소2최대4이다_test() {
+        String value = "ssar";
+        boolean result = Pattern.matches("^[a-zA-Z]{2,4}$", value); // ^: 안에 있으면 not의 의미
+        System.out.println("테스트 : " + result);
+    }
+
+    // username, email, fullname
+    @Test
+    public void user_username_test() {
+        String username = "ssar";
+        // 영문/숫자 2~20자 이내로 작성해주세요
+        boolean result = Pattern.matches("^[a-zA-Z0-9]{2,20}$", username);
+        System.out.println("테스트 : " + result);
+    }
+
+    @Test
+    public void user_fullname_test() throws Exception {
+        String fullname = "쌀";
+        // 영어/한글, 1자~20자
+        boolean result = Pattern.matches("^[a-zA-Z가-힣]{1,20}$", fullname);
+        System.out.println("테스트 : " + result);
+    }
+
+    @Test
+    public void user_email_test() throws Exception {
+        String email = "ssarj@nate.com"; // ac.kr co.kr or.kr X
+        // 영어,한글, 1자~20자
+        boolean result = Pattern.matches("^[a-zA-Z0-9]{2,10}@[a-zA-Z0-9]{2,6}\\.[a-zA-Z]{2,3}$", email);
+        System.out.println("테스트 : " + result);
+    }
+
+
+}
