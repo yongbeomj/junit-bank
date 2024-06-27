e0cfca1fe0dff8e014942e65718efb859fd7519a
Yongbeom Jang <alex.ybjang@gmail.com>
2024-06-27 10:51:51 +0900

    UserController 생성

---
+++ src/main/java/shop/mtcoding/bank/web/UserController.java
@@ -0,0 +1,29 @@
+package shop.mtcoding.bank.web;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import shop.mtcoding.bank.dto.ResponseDto;
+import shop.mtcoding.bank.dto.user.UserReqDto.JoinReqDto;
+import shop.mtcoding.bank.service.UserService;
+
+import javax.validation.Valid;
+
+import static shop.mtcoding.bank.dto.user.UserRespDto.JoinRespDto;
+
+@RequiredArgsConstructor
+@RequestMapping("/api")
+@RestController
+public class UserController {
+    private final UserService userService;
+
+    @PostMapping("/join")
+    public ResponseEntity<?> join(@RequestBody @Valid JoinReqDto joinReqDto) {
+        JoinRespDto joinRespDto = userService.회원가입(joinReqDto);
+        return new ResponseEntity<>(new ResponseDto<>(1, "회원가입 성공", joinRespDto), HttpStatus.CREATED);
+    }
+}
