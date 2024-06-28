c9ea93b96de4aaf006626031dfd54aa19fa375b6
Yongbeom Jang <alex.ybjang@gmail.com>
2024-06-28 22:15:56 +0900

    JWT 인증 실패 테스트 - unsuccessfulAuthentication

--- src/test/java/shop/mtcoding/bank/config/jwt/JwtAuthenticationFilterTest.java
+++ src/test/java/shop/mtcoding/bank/config/jwt/JwtAuthenticationFilterTest.java
@@ -10,6 +10,7 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.transaction.annotation.Transactional;
 import shop.mtcoding.bank.config.dummy.DummyObject;
 import shop.mtcoding.bank.domain.user.UserRepository;
 import shop.mtcoding.bank.dto.user.UserReqDto.LoginReqDto;
@@ -19,6 +20,8 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+// 테스트는 독립적이어야 하므로 테스트 순서 정하지 말 것!
+@Transactional // 테스트에서 기본값 롤백. 2개 테스트 진행할 때마다 BeforeEach가 실행되어 중복되므로 롤백 필요.
 @ActiveProfiles("test")
 @AutoConfigureMockMvc
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
@@ -60,16 +63,28 @@ public class JwtAuthenticationFilterTest extends DummyObject {
         assertNotNull(jwtToken); // token not null 확인
         assertTrue(jwtToken.startsWith(JwtVO.TOKEN_PREFIX)); // token 접두어(Bearer) 확인
         resultActions.andExpect(jsonPath("$.data.username").value("ssar")); // 데이터 확인 (username == ssar)
-    }
+    } // 테스트 후 롤백
 
     // 인증 실패
     @Test
     public void unsuccessfulAuthentication_test() throws Exception {
         // given
+        LoginReqDto loginReqDto = new LoginReqDto();
+        loginReqDto.setUsername("ssar");
+        loginReqDto.setPassword("12345");
+
+        String requestBody = om.writeValueAsString(loginReqDto);
+        System.out.println("테스트 : " + requestBody);
 
         // when
+        ResultActions resultActions = mvc.perform(post("/api/login").content(requestBody).contentType(MediaType.APPLICATION_JSON));
+        String responseBody = resultActions.andReturn().getResponse().getContentAsString();
+        String jwtToken = resultActions.andReturn().getResponse().getHeader(JwtVO.HEADER);
+
+        System.out.println("테스트 : " + responseBody);
+        System.out.println("테스트 : " + jwtToken);
 
         // then
-
+        resultActions.andExpect(status().isUnauthorized()); // http status 401 확인
     }
 }
