7ef64452e59816bf44713b50745d3b0f43e9d23f
Yongbeom Jang <alex.ybjang@gmail.com>
2024-06-29 22:46:22 +0900

    계좌목록 서비스 생성

--- src/main/java/shop/mtcoding/bank/domain/account/AccountRepository.java
+++ src/main/java/shop/mtcoding/bank/domain/account/AccountRepository.java
@@ -2,6 +2,7 @@ package shop.mtcoding.bank.domain.account;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface AccountRepository extends JpaRepository<Account, Long> {
@@ -10,5 +11,9 @@ public interface AccountRepository extends JpaRepository<Account, Long> {
     // select * from account where number = :number
     // checkpoint : 리팩토링 필요 (계좌 소유자 확인 시 쿼리가 2번 나가기 때문에 join fetch)
     Optional<Account> findByNumber(Long number);
-    
+
+    // jpa query method
+    // select * from account where user_id = :id
+    List<Account> findByUser_Id(Long id);
+
 }
--- src/main/java/shop/mtcoding/bank/service/AccountService.java
+++ src/main/java/shop/mtcoding/bank/service/AccountService.java
@@ -1,6 +1,8 @@
 package shop.mtcoding.bank.service;
 
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import shop.mtcoding.bank.domain.account.Account;
@@ -11,7 +13,10 @@ import shop.mtcoding.bank.dto.account.AccountReqDto.AccountSaveReqDto;
 import shop.mtcoding.bank.dto.account.AccountRespDto.AccountSaveRespDto;
 import shop.mtcoding.bank.handler.ex.CustomApiException;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
@@ -39,4 +44,42 @@ public class AccountService {
         // DTO 응답
         return new AccountSaveRespDto(accountPS);
     }
+
+    public AccountListRespDto 계좌목록보기_유저별(Long userId) {
+        User userPS = userRepository.findById(userId).orElseThrow(
+                () -> new CustomApiException("유저를 찾을 수 없습니다"));
+
+        // 유저의 모든 계좌목록
+        List<Account> accountListPS = accountRepository.findByUser_Id(userId);
+        return new AccountListRespDto(userPS, accountListPS);
+    }
+
+    @Setter
+    @Getter
+    public static class AccountListRespDto {
+        private String fullname;
+        private List<AccountDto> accountDtos = new ArrayList<>();
+
+        public AccountListRespDto(User user, List<Account> accounts) {
+            this.fullname = user.getFullname();
+            // this.accountDtos = accounts.stream().map((account) -> new AccountDto(account)).collect(Collectors.toList());
+            this.accountDtos = accounts.stream().map(AccountDto::new).collect(Collectors.toList()); // [account, account]
+        }
+
+        @Setter
+        @Getter
+        public class AccountDto {
+            private Long id;
+            private Long number;
+            private Long balance;
+
+            public AccountDto(Account account) {
+                this.id = account.getId();
+                this.number = account.getNumber();
+                this.balance = account.getBalance();
+            }
+        }
+    }
+
+
 }
