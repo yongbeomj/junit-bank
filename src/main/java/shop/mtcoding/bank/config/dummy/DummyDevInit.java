c442de34aa3158b7c18feed6d732b9395d52b5cb
Yongbeom Jang <alex.ybjang@gmail.com>
2024-07-04 17:33:04 +0900

    dev모드 초기 데이터 수정

--- src/main/java/shop/mtcoding/bank/config/dummy/DummyDevInit.java
+++ src/main/java/shop/mtcoding/bank/config/dummy/DummyDevInit.java
@@ -6,6 +6,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import shop.mtcoding.bank.domain.account.Account;
 import shop.mtcoding.bank.domain.account.AccountRepository;
+import shop.mtcoding.bank.domain.transaction.Transaction;
+import shop.mtcoding.bank.domain.transaction.TransactionRepository;
 import shop.mtcoding.bank.domain.user.User;
 import shop.mtcoding.bank.domain.user.UserRepository;
 
@@ -14,13 +16,29 @@ public class DummyDevInit extends DummyObject {
 
     @Profile("dev") // prod 모드에서 실행되면 안되므로 실행 서버 제한
     @Bean
-    CommandLineRunner init(UserRepository userRepository, AccountRepository accountRepository) {
+    CommandLineRunner init(UserRepository userRepository, AccountRepository accountRepository, TransactionRepository transactionRepository) {
         return (args -> {
             // 서버 실행 시 무조건 실행
             User ssar = userRepository.save(newUser("ssar", "쌀"));
-            User cos = userRepository.save(newUser("cos", "코스"));
-            Account ssarAccount = accountRepository.save(newAccount(1111L, ssar));
+            User cos = userRepository.save(newUser("cos", "코스,"));
+            User love = userRepository.save(newUser("love", "러브"));
+            User admin = userRepository.save(newUser("admin", "관리자"));
+
+            Account ssarAccount1 = accountRepository.save(newAccount(1111L, ssar));
             Account cosAccount = accountRepository.save(newAccount(2222L, cos));
+            Account loveAccount = accountRepository.save(newAccount(3333L, love));
+            Account ssarAccount2 = accountRepository.save(newAccount(4444L, ssar));
+
+            Transaction withdrawTransaction1 = transactionRepository
+                    .save(newWithdrawTransaction(ssarAccount1, accountRepository));
+            Transaction depositTransaction1 = transactionRepository
+                    .save(newDepositTransaction(cosAccount, accountRepository));
+            Transaction transferTransaction1 = transactionRepository
+                    .save(newTransferTransaction(ssarAccount1, cosAccount, accountRepository));
+            Transaction transferTransaction2 = transactionRepository
+                    .save(newTransferTransaction(ssarAccount1, loveAccount, accountRepository));
+            Transaction transferTransaction3 = transactionRepository
+                    .save(newTransferTransaction(cosAccount, ssarAccount1, accountRepository));
         });
     }
 }
--- src/main/java/shop/mtcoding/bank/domain/account/Account.java
+++ src/main/java/shop/mtcoding/bank/domain/account/Account.java
@@ -36,6 +36,8 @@ public class Account {
     @ManyToOne(fetch = FetchType.LAZY) // account.getUser().아무필드호출() == Lazy 발동
     private User user;
 
+    // 양방향 매핑 - 쿼리를 보면 맘에 안들게 나올 것
+
     @CreatedDate // Insert
     @Column(nullable = false)
     private LocalDateTime createdAt;
