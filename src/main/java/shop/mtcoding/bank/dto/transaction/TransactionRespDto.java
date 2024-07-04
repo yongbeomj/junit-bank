bbfd90fd5fe30e26351fe4d59f8012cd69f5f19e
Yongbeom Jang <alex.ybjang@gmail.com>
2024-07-04 15:57:35 +0900

    입출금내역조회 서비스 생성

---
+++ src/main/java/shop/mtcoding/bank/dto/transaction/TransactionRespDto.java
@@ -0,0 +1,62 @@
+package shop.mtcoding.bank.dto.transaction;
+
+import lombok.Getter;
+import lombok.Setter;
+import shop.mtcoding.bank.domain.account.Account;
+import shop.mtcoding.bank.domain.transaction.Transaction;
+import shop.mtcoding.bank.util.CustomDateUtil;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class TransactionRespDto {
+
+    @Setter
+    @Getter
+    public static class TransactionListRespDto {
+        private List<TransactionDto> transactionDtos = new ArrayList<>();
+
+        public TransactionListRespDto(Account account, List<Transaction> transactions) {
+            this.transactionDtos = transactions.stream()
+                    .map(transaction -> new TransactionDto(transaction, account.getNumber()))
+                    .collect(Collectors.toList());
+        }
+
+        @Setter
+        @Getter
+        public class TransactionDto {
+            private Long id;
+            private String gubun;
+            private Long amount;
+            private String sender;
+            private String receiver;
+            private String tel;
+            private String createdAt;
+            private Long balance;
+
+            public TransactionDto(Transaction transaction, Long accountNumber) {
+                this.id = transaction.getId();
+                this.gubun = transaction.getGubun().getValue();
+                this.amount = transaction.getAmount();
+                this.sender = transaction.getSender();
+                this.receiver = transaction.getReceiver();
+                this.tel = transaction.getTel() == null ? "없음" : transaction.getTel();
+                this.createdAt = CustomDateUtil.toStringFormat(transaction.getCreatedAt());
+
+                if (transaction.getDepositAccount() == null) {
+                    this.balance = transaction.getWithdrawAccountBalance();
+                } else if (transaction.getWithdrawAccount() == null) {
+                    this.balance = transaction.getDepositAccountBalance();
+                } else {
+                    // 1111 계좌의 입출금 내역 (출금계좌 = 값, 입금계좌 = 값)
+                    if (accountNumber.longValue() == transaction.getDepositAccount().getNumber()) {
+                        this.balance = transaction.getDepositAccountBalance();
+                    } else {
+                        this.balance = transaction.getWithdrawAccountBalance();
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ src/main/java/shop/mtcoding/bank/service/TransactionService.java
@@ -0,0 +1,34 @@
+package shop.mtcoding.bank.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import shop.mtcoding.bank.domain.account.Account;
+import shop.mtcoding.bank.domain.account.AccountRepository;
+import shop.mtcoding.bank.domain.transaction.Transaction;
+import shop.mtcoding.bank.domain.transaction.TransactionRepository;
+import shop.mtcoding.bank.handler.ex.CustomApiException;
+
+import java.util.List;
+
+import static shop.mtcoding.bank.dto.transaction.TransactionRespDto.TransactionListRespDto;
+
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+@Service
+public class TransactionService {
+
+    private final AccountRepository accountRepository;
+    private final TransactionRepository transactionRepository;
+
+    public TransactionListRespDto 입출금목록보기(Long userId, Long accountNumber, String gubun, int page) {
+        Account accountPS = accountRepository.findByNumber(accountNumber)
+                .orElseThrow(() -> new CustomApiException("해당 계좌를 찾을 수 없습니다"));
+
+        accountPS.checkOwner(userId);
+
+        List<Transaction> transactionListPS = transactionRepository.findTransactionList(accountPS.getId(), gubun, page);
+        return new TransactionListRespDto(accountPS, transactionListPS);
+    }
+}
+
